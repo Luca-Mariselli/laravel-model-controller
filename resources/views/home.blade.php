c1246063414e1c919a197cd29310a8d3382c5dbe
Luca-Mariselli <lucamariselli@gmail.com>
2024-07-02 23:51:48 +0200

    aggiunto margin

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Film:</h1>
-    @foreach ($movies as $movie)
-        <p>{{ $movie['title'] }}</p>
-    @endforeach
+    <div class="ms-3">
+        <h1>Film:</h1>
+        @foreach ($movies as $movie)
+            <p>{{ $movie['title'] }}</p>
+        @endforeach
+    </div>
 @endsection
