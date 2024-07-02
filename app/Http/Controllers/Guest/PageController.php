968bce421ffcbf81c35aac436161de2d8063f739
Luca-Mariselli <lucamariselli@gmail.com>
2024-07-02 23:50:57 +0200

    Creato model movie, controller e stampati in pagina tutti i titoli dei film

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Movie;
+
+class PageController extends Controller
+{
+    public function home() {
+        $movies = Movie::all();
+
+        $dati = [
+            "movies" => $movies
+        ];
+
+        return view('home', $dati);
+    }
+}
